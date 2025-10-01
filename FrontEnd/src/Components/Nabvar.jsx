930aec38d37443d376791e9e4f4569f414f56580
Anas Ibn Belal <anasibnebelal400@gmail.com>
2025-10-01 21:44:46 +0600

    Login Feature Implementing

--- FrontEnd/src/Components/Nabvar.jsx
+++ FrontEnd/src/Components/Nabvar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-center items-center w-full bg-gradient-to-r from-slate-900 to-slate-800 shadow-lg border-b border-slate-700">
       <nav className="max-w-6xl w-full px-6 py-4 flex items-center justify-between">
-        <Link to="/auth">
+        <Link to="/auth/login">
           <Button className="bg-green-500 dark:bg-green-500 dark:text-white dark:hover:bg-green-800" variant="default" iconLeft={<Fingerprint />}>
             Login
           </Button>
